0ba432af97cf97a3529fb638cfc15ad32078f26e
Steven Ge <xijin.ge@sdstate.edu>
2023-02-22 17:29:01 -0600

    Update app.R

--- app.R
+++ app.R
@@ -1,14 +1,3 @@
-options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
-
-# Comment this if you don't want the app to be served on a random port
-options(shiny.port = httpuv::randomPort())  
-
-# Detach all loaded packages and clean your environment
-golem::detach_all_attached()
-# rm(list=ls(all.names = TRUE))
-
-# Document and reload your package
-golem::document_and_reload()
-
-# Run the application
-run_app()
+pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+options( "golem.app.prod" = TRUE)
+RTutor::run_app() # add parameters here (if any)
