3736c05c3b4f514e4538c281af71da61e4fcf6a0
Steven Ge <xijin.ge@sdstate.edu>
2023-11-08 10:53:38 -0600

    back to GPT-4

--- R/fct_helpers.R
+++ R/fct_helpers.R
@@ -22,7 +22,7 @@ max_query_length <- 2000 # max # of characters
 #language_model <- "code-davinci-002	"# "text-davinci-003"
 language_models <- c("gpt-4-1106-preview", "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-0301", "gpt-4", "gpt-4-0314", "text-davinci-003")
 names(language_models) <- c("GPT-4 Turbo (11/23)", "ChatGPT", "ChatGPT 16k", "ChatGPT (03/23)", "GPT-4", "GPT-4 (03/23)", "Davinci")
-default_model <- "GPT-4 Turbo (11/23)" #"GPT-4 (03/23)" # "ChatGPT" 
+default_model <-  "GPT-4 (03/23)" # "ChatGPT" # "GPT-4 Turbo (11/23)"
 max_content_length <- 3000 # max tokens:  Change according to model !!!!
 max_content_length_ask <- 3000 # max tokens:  Change according to model !!!!
 default_temperature <- 0.2
